462a4659f58a8f0d2a4685d76073332e28e71f2e
AmazinAxel <axel@amazinaxel.com>
2024-12-30 09:22:21 -0800

    Refactor media button

--- home-manager/ags/widgets/modules/media.tsx
+++ home-manager/ags/widgets/modules/media.tsx
@@ -3,19 +3,23 @@ import Mpris from 'gi://AstalMpris'
 
 const mpris = Mpris.get_default();
 
+const mediaBtn = () => 
+    <button
+        onClick={() => mpris.players[0].play_pause()}
+        onScroll={(_, e) => (e.delta_y > 0) ? mpris.players[0].next() : mpris.players[0].previous()}
+        hexpand cursor="pointer">
+        <icon icon="emblem-music-symbolic"/>
+    </button>
+
 export const Media = () => {
     bind(mpris, "players").as((players) => {
+        console.log(players.length)
         if (players.length < 1)
-            return ''; // Don't show anything if no player to control
+            return <box/>; // Don't show anything if no player to control
         
-        return (
-            <button
-                onClick={() => mpris.players[0].play_pause()}
-                onScroll={(_, e) => (e.delta_y > 0) ? mpris.players[0].next() : mpris.players[0].previous()}
-                hexpand cursor="pointer">
-                <icon icon="emblem-music-symbolic"/>
-            </button>
-        );
+        return mediaBtn()
     });
-    return <box/>; // Fix LSP type issue
+
+    // Bind doesn't run on start - fallback
+    return (mpris.players.length) ? mediaBtn() : <box/>;
 };
