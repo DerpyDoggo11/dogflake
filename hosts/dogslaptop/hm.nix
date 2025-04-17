33b7f0dfdda507a8d82244ae5140b2d72067ae4a
dog <vrs18451@gmail.com>
2025-04-16 22:15:49 -0700

    removed autostart apps

--- hosts/dogslaptop/hm.nix
+++ hosts/dogslaptop/hm.nix
@@ -1,26 +1,23 @@
-{
+{ # add this back for another monitor
   wayland.windowManager.hyprland.settings = {
-    monitor = [ # https://wiki.hyprland.org/Configuring/Monitors/
-      "        , preferred,     auto,     auto"
-      "HDMI-A-1, 1920x1080@144, auto-left,  auto"
-    ];
+    #monitor = [ # https://wiki.hyprland.org/Configuring/Monitors/
+    #  "        , preferred,     auto,     auto"
+    #  "HDMI-A-1, 1920x1080@144, auto-left,  auto"
+    #];
 
     exec-once = [ # Autostart apps
-      "[workspace 3 silent] microsoft-edge"
-      "[workspace 4 silent] teams-for-linux"
-      "[workspace 6 silent] thunderbird"
+      #"[workspace 4 silent] discord"
+      #"[workspace 5 silent] teams-for-linux"
+      #"[workspace 6 silent] thunderbird"
     ];
 
     workspace = [
       # Monitor handling (supports up to two monitors: main screen on left/above, laptop screen on right/bottom)
-      "1, monitor:HDMI-A-1"
-      "2, monitor:HDMI-A-1"
-      "3, monitor:HDMI-A-1"
-      "4, monitor:HDMI-A-1"
-      "5, monitor:DP-1"
+      #"1, monitor:HDMI-A-1"
+      #"2, monitor:HDMI-A-1"
+      #"3, monitor:HDMI-A-1"
+      #"4, monitor:HDMI-A-1"
+      #"5, monitor:DP-1"
     ];
-
-    # Custom side mouse key for quick screenshots
-    bind = [ "Super, D, exec, screenshot" ];
   };
 }
